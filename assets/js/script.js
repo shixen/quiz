cc6cde5c9f5f9129aafdffbca92faca3cc9177e4
niklas eriksson <niklaseriksson32@hotmail.com>
2024-01-06 12:59:17 +0000

    add html content

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+let questions = [
+    {
+        question: 'What was the largest animal to ever live ?',
+        a: 'T-rex',
+        b: 'Mammuth',
+        c: 'Blue whale',
+        d: 'Shark',
+        correct: 'c'
+        
+    }, {
+        question:'What aniaml causes the most deaths today?',
+        a: 'Crocodiles',
+        b: 'Sharks',
+        c: 'Mosquitoes',
+        d: 'Snakes',
+        correct: 'c'
+    }, {
+        question: 'How fast can a cheetah run ?',
+        a: '50 km/h',
+        b: '70 km/h',
+        c: '100 km/h',
+        d: '130 km/h',
+        correct: 'd'
+
+    },{
+        question: 'What animal is the most protected today?',
+        a: 'Amur leopard',
+        b: 'Tiger',
+        c: 'Rhino',
+        d: 'Gorilla',
+        correct: 'a'
+    },{
+        question: 'What animal live the longest ?',
+         a: 'Turtles',
+         b: 'Parrots',
+         c: 'Quahog clam',
+         d: 'Lobsters',
+         correct: 'c'
+}
+]
